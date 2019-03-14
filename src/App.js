9273536a1cfa18d6d9b06a0ee9ae1fc723be2151
Marius Nalivaika <marius.nalivaika@gmail.com>
2019-03-14 05:33:12 +0000

    Created All files
    Just CSS issue

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Containers/Header";
+import MemeGenerator from "./Components/MemeGenerator";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MemeGenerator.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemes: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        const { memes } = response.data;
+        this.setState({ allMemes: memes });
+      });
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form action="" className="meme-form">
+          <input
+            type="text"
+            placeholder="Top Text"
+            name="topText"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Bottom Text"
+            name="bottomText"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+          <button>GEN</button>
+        </form>
+        <div className="meme" />
+        <img src={this.state.randomImg} alt="" />
+        <h1 className="top">{this.state.topText}</h1>
+        <h1 className="bottom">{this.state.bottomText}</h1>
+      </div>
+    );
+  }
+}
+
+export default MemeGenerator;
